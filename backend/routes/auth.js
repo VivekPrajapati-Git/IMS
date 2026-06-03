9239f5f5513780df5be5c99b9da9c1cd9f644b1e
VivekPrajapati-Git <vivekbusiness20@gmail.com>
2026-06-03 12:37:59 +0530

    Added : Login Route Done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const bcrypt = require('bcrypt')
 const debug = require('debug')('auth_router')
 const db = require('../connections/supabase_conn')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 // SignUp Routes
 router.post('/signUp', async (req, res) => {
@@ -36,7 +38,50 @@ router.post('/signUp', async (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    console.log("Hello")
+    try {
+        const {UserName , Password} = req.body
+
+        const { data: user, error } = await db
+            .from("Login")
+            .select("*")
+            .eq("username", UserName)
+            .single();
+
+        if(error){
+            console.log(error.message)
+            res.send(error.message)
+        }
+
+        if(!user){
+            res.send("User Not Found").status(401)
+        }
+
+        const isMatch = bcrypt.compare(Password,user.saltedpassword)
+
+        if(!isMatch){
+            res.send("Invalid Password").status(401)
+        }
+
+        const secret = process.env.JWT_SECRET
+        const token = jwt.sign(
+            {
+                id : user.uniqueid || user.UniqueID,
+                username : user.UserName || user.username,
+                role : user.role || user.Role
+            },
+            secret,
+            { expiresIn : "24h"}
+        )
+
+        res.json({
+            "Message" : "Login Successful",
+            token : token,
+            role : user.Role || user.role
+        })
+    } catch(err){
+        console.log(err)
+        res.send("Internal server error").status(500)
+    }
 })
 
 module.exports = router
